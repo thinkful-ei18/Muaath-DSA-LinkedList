7fed7bb997cc75f30918b830663ce9035c378df0
admin <Mistermo716@gmail.com>
2018-04-05 09:01:46 -0700

    Drills

---
+++ drills.js
@@ -0,0 +1,17 @@
+'use strict';
+const LinkedList = require('./linkedlist');
+
+function main() {
+  let SLL = new LinkedList();
+
+  SLL.insertFirst('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  SLL.insertLast('Tauhida');
+  SLL.remove('squirrel');
+  console.log(SLL);
+}
+
+main();
